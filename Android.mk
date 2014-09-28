1de30bd873054f27f565fbc84f33fef08b3a338d
Jake Weinstein <xboxlover360@gmail.com>
2014-09-28 17:40:28 -0500

    perf: fix linking of qc-opt, qc-sqlite, and qc-dalvik
    Leave skia broken as that static lib is old and likely does not work anymore due to the large update in January
    
    Change-Id: Ib646fdc1f7ce807943dc23556791a92bdc554a26

--- Android.mk
+++ Android.mk
@@ -4,10 +4,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libqc-sqlite
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER         := qcom
+LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_CLASS  := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
 LOCAL_SRC_FILES := libqc-sqlite.a
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 
@@ -20,16 +23,23 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libqc-dalvik
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER         := qcom
+LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_CLASS  := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
 LOCAL_SRC_FILES := libqc-dalvik.a
-
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libqc-opt
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER  := qcom
+LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_CLASS  := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
 LOCAL_SRC_FILES := libqc-opt.so
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/obj/STATIC_LIBRARIES
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_PREBUILT)
+
 endif
